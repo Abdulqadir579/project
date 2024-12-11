f2089f02adef32ef512a43e1208ae0c3caa5c5e1
Abdulqadir579 <abdulqadirk1998@gmail.com>
2024-12-11 20:36:15 +0500

    this is commit bro

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import forms from '@tailwindcss/forms';
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -20,6 +22,6 @@ export default {
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
+    forms,
   ],
 };
